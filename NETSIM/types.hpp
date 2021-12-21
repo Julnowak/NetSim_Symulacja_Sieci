1f4e789ec6c4aaf1da39359e605de445dc2cc6fd
Julnowak <91898403+Julnowak@users.noreply.github.com>
2021-12-21 13:56:01 +0100

    Final - Part one
    Added size_type and ElementID. I used unsigned int, because values are always bigger than 0.

--- NETSIM/types.hpp
+++ NETSIM/types.hpp
@@ -5,6 +5,7 @@
 #ifndef NETSIM_TYPES_HPP
 #define NETSIM_TYPES_HPP
 
-using ElementID = int;
+using ElementID = unsigned int;     // Używamy, gdyż liczby nie są ujemne
+using size_type = unsigned int;     // typ pomocniczy, określający  liczbę półproduktów w kontenerze
 
 #endif //NETSIM_TYPES_HPP
