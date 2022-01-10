5673567fc41303534bbe2ccdc5d04ed93fe806e6
Julnowak <91898403+Julnowak@users.noreply.github.com>
2022-01-10 15:17:56 +0100

    to be checked/changed

--- NETSIM/include/factory.hpp
+++ NETSIM/include/factory.hpp
@@ -5,4 +5,87 @@
 #ifndef NETSIM_FACTORY_HPP
 #define NETSIM_FACTORY_HPP
 
+#include "nodes.hpp"
+#include "types.hpp"
+
+#include <algorithm>
+#include <exception>
+
+template <typename Node>
+class NodeCollection{
+public:
+    using container_t = typename std::list<Node>;
+    using iterator = typename container_t::iterator;
+    using const_iterator = typename container_t::const_iterator;
+
+    void add(Node &node){Nodes.push_back(std::move(node));}
+    void remove_by_id(ElementID _id);
+    iterator find_by_id(ElementID _id) {auto it = std::find_if(Nodes.begin(), Nodes.end(),[_id](const auto& elem){ return (elem.get_id() == _id);}); return it;}
+    const_iterator find_by_id(ElementID _id) const {auto it = std::find_if(Nodes.begin(), Nodes.end(),[_id](const auto& elem){return (elem.get_id() == _id);}); return it;}
+
+    // iteratory
+    iterator begin() {return Nodes.begin();}
+    iterator end() {return Nodes.end();}
+    const_iterator cbegin(){return Nodes.cbegin();}
+    const_iterator cend(){return Nodes.cend();}
+
+private:
+    container_t Nodes;
+};
+
+
+template<typename Node>
+void NodeCollection<Node>::remove_by_id(ElementID _id)
+{
+    NodeCollection::iterator it = find_by_id(_id);
+    if(it != Nodes.end()) Nodes.erase(it);
+}
+
+
+class Factory{
+public:
+    bool is_consistent();
+    void do_deliveries(Time time) {std::for_each(Ramps.begin(),Ramps.end(),[time](Ramp& ramp){ramp.deliver_goods(time);});}
+    void do_package_passing();
+    void do_work(Time time) {std::for_each(Workers.begin(),Workers.end(),[time](Worker& worker){worker.do_work(time);});}
+
+    // RAMP
+    void add_ramp(Ramp&& node){Ramps.add(node);}
+    void remove_ramp(ElementID id){Ramps.remove_by_id(id);}
+
+    NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id) {return Ramps.find_by_id(id);}
+    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id) const {return Ramps.find_by_id(id);}
+
+    NodeCollection<Ramp>::const_iterator ramp_cbegin() {return Ramps.cbegin();}
+    NodeCollection<Ramp>::const_iterator ramp_cend() {return Ramps.cend();}
+
+
+    // WORKER
+    void add_worker(Worker&& node){Workers.add(node);}
+    void remove_worker(ElementID id);
+
+    NodeCollection<Worker>::iterator find_worker_by_id(ElementID id) {return Workers.find_by_id(id);}
+    NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id) const {return Workers.find_by_id(id);}
+
+    NodeCollection<Worker>::const_iterator worker_cbegin() {return Workers.cbegin();}
+    NodeCollection<Worker>::const_iterator worker_cend() {return Workers.cend();}
+
+
+    // STOREHOUSE
+    void add_storehouse(Storehouse&& node){Storehouses.add(node);}
+    void remove_storehouse(ElementID id);
+
+    NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id) {return Storehouses.find_by_id(id);}
+    NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id) const {return Storehouses.find_by_id(id);}
+
+    NodeCollection<Storehouse>::const_iterator storehouse_cbegin() {return Storehouses.cbegin();}
+    NodeCollection<Storehouse>::const_iterator storehouse_cend() {return Storehouses.cend();}
+
+private:
+    NodeCollection<Ramp> Ramps;
+    NodeCollection<Worker> Workers;
+    NodeCollection<Storehouse> Storehouses;
+};
+
+
 #endif //NETSIM_FACTORY_HPP
--- NETSIM/include/nodes.hpp
+++ NETSIM/include/nodes.hpp
@@ -5,4 +5,139 @@
 #ifndef NETSIM_NODES_HPP
 #define NETSIM_NODES_HPP
 
+#include "types.hpp"
+#include "helpers.hpp"
+#include "storage_types.hpp"
+
+#include <iostream>
+#include <memory>
+#include <optional>
+#include <map>
+#include <functional>
+
+
+enum ReceiverType{
+    WORKER, STOREHOUSE
+};
+
+
+class IPackageReceiver{
+public:
+    using const_it = IPackageStockpile::const_iterator;
+
+    virtual void receive_package(Package&& p) = 0; //
+    virtual ElementID get_id() const = 0;
+    virtual ReceiverType get_receiver_type () = 0; //
+
+    // iteratory
+    virtual const_it begin() const = 0;
+    virtual const_it end() const = 0;
+    virtual const_it cbegin() const = 0;
+    virtual const_it cend() const = 0;
+};
+
+
+class Storehouse : public IPackageReceiver {
+public:
+    Storehouse(ElementID id, std::unique_ptr<IPackageStockpile> d = std::make_unique<PackageQueue>(PackageQueueType::LIFO)) : _id(id), _d(std::move(d)) {}
+
+    ReceiverType get_receiver_type() {return ReceiverType::STOREHOUSE;};
+    ElementID get_id() const { return _id; }
+    void receive_package(Package&& pack) { _d->push(std::move(pack)); }
+
+    // iteratory
+    const_it begin() const {return _d -> begin();};
+    const_it end() const {return _d -> end();};
+    const_it cbegin() const {return _d -> cbegin();};
+    const_it cend() const {return _d -> cend();};
+
+private:
+    ElementID _id;
+    std::unique_ptr<IPackageStockpile> _d;
+};
+
+
+class ReceiverPreferences{
+public:
+    using preferences_t = std::map<IPackageReceiver*, double>;
+    using const_iterator = preferences_t::const_iterator;
+    using reverse_iterator = preferences_t::reverse_iterator;
+
+    ReceiverPreferences(ProbabilityGenerator pg = default_probability_generator): _pg(pg) {}
+    void add_receiver(IPackageReceiver* r);
+    void remove_receiver(IPackageReceiver* r);
+    IPackageReceiver* choose_receiver();
+    const preferences_t& get_preferences() const {return preferences;}
+
+    const_iterator begin() const {return preferences.cbegin();}
+    const_iterator end() const {return preferences.cend();}
+    const_iterator cbegin() const {return preferences.cbegin();}
+    const_iterator cend() const {return preferences.cend();}
+    reverse_iterator rbegin() {return preferences.rbegin();}
+    reverse_iterator rend() {return preferences.rend();}
+
+private:
+    preferences_t preferences;
+    ProbabilityGenerator _pg;
+};
+
+
+class PackageSender{
+public:
+    //PackageSender(PackageSender&&);
+    void send_package();
+    std::optional<Package>& get_sending_buffer() {return bufor;}
+    //bool buffer_empty();
+    ReceiverPreferences receiver_preferences_;
+
+protected:
+    void push_package(Package&& pack) {bufor = std::move(pack);};
+
+private:
+    std::optional<Package> bufor;
+};
+
+
+class Ramp : public PackageSender {
+public:
+    Ramp(ElementID id, TimeOffset di): _id(id), _di(di) {}
+    void deliver_goods(Time t);
+    TimeOffset get_delivery_interval() {return _di;}
+    ElementID get_id() const { return _id; }
+private:
+    ElementID _id;
+    TimeOffset _di;
+};
+
+
+class Worker : public PackageSender, public IPackageReceiver{
+public:
+    Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q) : _id(id), _pd(pd), _q(std::move(q)), _t(0), working(std::nullopt) {}
+    ElementID get_id() const { return _id; }
+    void receive_package(Package&& pack) { _q->push(std::move(pack)); }
+    void do_work(Time t);
+    ReceiverType get_receiver_type() { return ReceiverType::WORKER; };
+
+    TimeOffset get_processing_duration () {return _pd;}
+    Time get_package_processing_start_time() {return _t;}
+
+    // iteratory
+    const_it begin() const { return _q->cbegin(); };
+    const_it end() const { return _q->cend(); };
+    const_it cbegin() const { return _q->cbegin(); };
+    const_it cend() const { return _q->cend(); };
+private:
+    ElementID _id;
+    TimeOffset _pd;
+    std::unique_ptr<IPackageQueue> _q;
+    Time _t;
+    std::optional<Package> working;
+};
+
+
+
+
+
+
+
 #endif //NETSIM_NODES_HPP
