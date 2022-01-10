6ff00c44a9b2c7a6c2b9d5823215be192f2e1b0a
Julnowak <91898403+Julnowak@users.noreply.github.com>
2022-01-10 15:18:28 +0100

    To be checked/changed

--- NETSIM/src/factory.cpp
+++ NETSIM/src/factory.cpp
@@ -2,4 +2,121 @@
 // Created by Julia on 01.01.2022.
 //
 
+
 #include "factory.hpp"
+
+struct no_reachable_storehouse_error : public std::exception
+{
+    const char * what () const throw (){return "No reachable storehouse";}
+};
+
+
+bool Factory::is_consistent()
+{
+    if(Ramps.begin()==Ramps.end()) return false;
+    if(Workers.begin()==Workers.end()) return false;
+    if(Storehouses.begin()==Storehouses.end()) return false;
+
+    bool consistent = true;
+
+    std::for_each
+    (Ramps.begin(),Ramps.end(),[&consistent](Ramp &ramp)
+    {
+        auto preferences=ramp.receiver_preferences_.get_preferences();
+        if(preferences.empty()) consistent=false;
+    });
+
+
+    std::for_each
+    (Workers.begin(),Workers.end(),[this,&consistent](Worker &worker)
+    {
+        auto id=worker.get_id();
+        bool no_empty = false;
+
+        std::for_each
+        (Ramps.begin(),Ramps.end(),[id,&no_empty](Ramp &ramp)
+        {
+            auto preferences= ramp.receiver_preferences_.get_preferences();
+            for(auto & preference : preferences)
+                if(preference.first->get_id()==id) no_empty = true;
+        });
+
+        std::for_each
+        (Workers.begin(),Workers.end(),[id,&no_empty](Worker &worker)
+        {
+            if(worker.get_id() != id)
+            {
+                auto preferences = worker.receiver_preferences_.get_preferences();
+                for (auto & preference : preferences)
+                    if (preference.first->get_id() == id) no_empty = true;
+            }
+        });
+
+        auto workerPreferences = worker.receiver_preferences_.get_preferences();
+        auto workerId = worker.get_id();
+        bool check = false;
+
+        for(auto & workerPreference : workerPreferences)
+        {
+            auto type=workerPreference.first->get_receiver_type();
+            if(workerPreference.first->get_id()!=workerId || type==STOREHOUSE) check = true;
+        }
+
+        if(!no_empty || !check) consistent = false;
+
+    });
+
+
+    std::for_each
+    (Storehouses.begin(),Storehouses.end(),[this,&consistent](Storehouse &storehouse)
+    {
+        auto id= storehouse.get_id();
+        bool no_empty = false;
+
+        std::for_each
+        (Workers.begin(),Workers.end(),[id,&no_empty](Worker &worker)
+        {
+            auto preferences=worker.receiver_preferences_.get_preferences();
+            for(auto & preference : preferences)
+                if(preference.first->get_id()==id) no_empty = true;
+        });
+
+        if(!no_empty) consistent = false;
+    });
+
+    return consistent;
+}
+
+
+void Factory::do_package_passing()
+{
+    std::for_each(Ramps.begin(),Ramps.end(),[](Ramp &ramp){ ramp.send_package();});
+    std::for_each(Workers.begin(),Workers.end(),[](Worker &worker){ worker.send_package();});
+}
+
+
+void Factory::remove_worker(ElementID id)
+{
+    std::for_each
+    (Ramps.begin(),Ramps.end(),[id](Ramp &ramp)
+    {
+        auto preferences=ramp.receiver_preferences_.get_preferences();
+        for(auto & preference : preferences)
+            if(preference.first->get_id()==id) ramp.receiver_preferences_.remove_receiver(preference.first);
+    });
+
+    Workers.remove_by_id(id);
+}
+
+void Factory::remove_storehouse(ElementID id)
+{
+    std::for_each
+    (Workers.begin(),Workers.end(),[id](Worker &worker)
+    {
+        auto preferences=worker.receiver_preferences_.get_preferences();
+            for(auto & preference : preferences)
+                if(preference.first->get_id()==id) worker.receiver_preferences_.remove_receiver(preference.first);
+    });
+
+    Storehouses.remove_by_id(id);
+}
--- NETSIM/src/nodes.cpp
+++ NETSIM/src/nodes.cpp
@@ -3,3 +3,98 @@
 //
 
 #include "nodes.hpp"
+#include "helpers.hpp"
+
+
+void Ramp::deliver_goods(Time t)
+{
+    if (t % _di == 1)
+    {
+        Package pack;
+        push_package(std::move(pack));
+    }
+}
+
+
+void Worker::do_work(Time t){
+
+    if (!working && !_q->empty())
+    {
+        working = _q->pop();
+        _t = t;
+    }
+
+    if (working && t - _t == _pd - 1)
+    {
+        push_package(std::move(*working));
+        working.reset();
+    }
+}
+
+
+void PackageSender::send_package()
+{
+    if (bufor)
+    {
+        IPackageReceiver* receiver = receiver_preferences_.choose_receiver();
+        receiver->receive_package(std::move(*bufor));
+        bufor.reset();
+    }
+}
+
+
+//bool PackageSender::buffer_empty(){
+//    if (bufor) return true;
+ //   else return false;
+//}
+
+void ReceiverPreferences::add_receiver(IPackageReceiver* r)
+{
+    double preference = 1.0;
+    double suma = 0.0;
+    preferences[r] = preference;
+    for (auto& pair : preferences)
+    {
+        preferences[pair.first] = 1.0 / static_cast<double>(preferences.size());
+        suma = suma + pair.second;
+    }
+    if (suma != 1.0) preferences[r] = preferences[r] + 1.0 - suma;
+
+}
+
+void ReceiverPreferences::remove_receiver(IPackageReceiver* r){
+    IPackageReceiver* copy_r = r;
+    double suma = 0.0;
+    preferences.erase(r);
+
+    if(!preferences.empty())
+        {
+            for (const auto &pair : preferences)
+            {
+                preferences[pair.first]= 1.0 / static_cast<double>(preferences.size());
+                suma = suma + pair.second;
+                copy_r  = pair.first;
+            }
+
+            if (suma!= 1.0) preferences[copy_r ] = preferences[copy_r ] + 1.0 - suma;
+        }
+}
+
+
+IPackageReceiver* ReceiverPreferences::choose_receiver()
+{
+    double los = default_probability_generator();
+    double dis = 0.0;
+    IPackageReceiver* helper = preferences.rbegin()->first;
+
+    std::map<IPackageReceiver*, double>::reverse_iterator it;
+
+    for (it = preferences.rbegin(); it != preferences.rend(); ++it)
+    {
+        dis = dis + it->second;
+        helper = it->first;
+        if (los < dis) break;
+    }
+
+    return helper;
+}
