0a2e889048c6a4655505ed2ea2cecc2c5b6a243d
Julnowak <91898403+Julnowak@users.noreply.github.com>
2022-01-16 21:20:38 +0100

    Final 16.01

---
+++ NETSIM/include/simulation.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Julia on 10.01.2022.
+//
+
+#ifndef NETSIM_SIMULATION_HPP
+#define NETSIM_SIMULATION_HPP
+
+#include "factory.hpp"
+#include "../config.hpp"
+
+void simulate(Factory& f, TimeOffset d, std::function<void(Factory&,Time)>);
+
+#endif //NETSIM_SIMULATION_HPP
